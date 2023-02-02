b010fe5e8ccb74ad35ddb085d47c804e88fc84f1
Anmol Malik <anmolmalik97new@gmail.com>
2023-02-02 15:30:04 +0530

    changes

--- jobs/generateConsolidatedReport.job.js
+++ jobs/generateConsolidatedReport.job.js
@@ -1,20 +1,22 @@
 const axios = require("axios");
 const moment = require("moment");
 const mongoose = require("mongoose");
-const ejs = require('ejs');
-const fs = require('fs');
-const path = require('path');
+const ejs = require("ejs");
+const fs = require("fs");
+const path = require("path");
 const pdf = require("html-pdf");
 
-const shrutiPatient = require('./addShrutiPatient.job')
-const ScreeningModel = require('../models/campScreening.model');
-const Patient = require('../models/patientRecord');
-const labItemModel = require('../models/labItem');
+const shrutiPatient = require("./addShrutiPatient.job");
+const ScreeningModel = require("../models/campScreening.model");
+const Patient = require("../models/patientRecord");
+const labItemModel = require("../models/labItem");
 const { uploadEJSPDF, getEjsFile } = require("../utils/upload");
 const { pdfMerge, getFileBufferFromUrl } = require("../utils/pdfMerge");
-const { tranformerConsolidatedReportData } = require("../utils/consolidatedReport");
+const {
+  tranformerConsolidatedReportData,
+} = require("../utils/consolidatedReport");
 const campsModel = require("../models/camps.model");
-const ObjectId = require('mongoose').Types.ObjectId;
+const ObjectId = require("mongoose").Types.ObjectId;
 
 const renderHTML = fs.readFileSync(
   path.resolve(__dirname, "../views/consolidatedReport.ejs"),
@@ -23,25 +25,23 @@ const renderHTML = fs.readFileSync(
 
 const render = ejs.compile(renderHTML);
 
-
-
 module.exports = async (req, res) => {
   console.log("Consolidated Report Fetching");
   try {
-    let urlMaps = {}
+    let urlMaps = {};
     let detailsMap = {};
     const patientIds = [];
     const today = moment().toISOString();
     const dateFilter = {
-      createdAt: {
-        $gte: moment().subtract(7, 'days').startOf('days').toDate(),
-        $lte: moment().subtract(5, 'days').endOf('days').toDate(),
-      }
+      // createdAt: {
+      //   $gte: moment().subtract(7, "days").startOf("days").toDate(),
+      //   $lte: moment().subtract(5, "days").endOf("days").toDate(),
+      // },
       // createdAt: {
       //   $gte: moment().subtract(5, 'days').startOf('days').toDate(),
       //   $lte: moment(today).endOf('day').toDate()
       // },
-    }
+    };
 
     const screenings = await ScreeningModel.find({
       ...dateFilter,
@@ -49,10 +49,12 @@ module.exports = async (req, res) => {
       // patientId: ObjectId("63ce2592d5b7a02a456dde29") // hemant C
       // patientId: ObjectId("63ce417ce34e6e564f3c64f0") // hemant C
       // patientId: ObjectId("63ce2690d5b7a02a456dde9f") //atha
+    })
+      .populate([{ path: "patientId" }, { path: "campId" }])
+      .sort({ createdAt: "asc" })
+      .exec();
 
-    }).populate([{ path: 'patientId' }, { path: 'campId' }]).sort({ createdAt: 'asc' }).exec();
-
-    console.log(screenings.length, 'screenings')
+    console.log(screenings.length, "screenings");
 
     if (screenings) {
       for (const screening of screenings) {
@@ -61,7 +63,8 @@ module.exports = async (req, res) => {
         }
         const uhid = screening?.patientId?.uhid;
         detailsMap = {
-          ...detailsMap, [uhid]: {
+          ...detailsMap,
+          [uhid]: {
             ...(detailsMap[uhid] || {}),
             campId: screening?.campId,
             state: screening?.campId?.stateName,
@@ -69,55 +72,66 @@ module.exports = async (req, res) => {
             location: `${screening?.campId?.talukaName},${screening?.campId?.villageName}(${screening?.campId?.villagePinCode})`,
             patient: screening?.patientId,
             screeningDate: screening?.createdAt,
-            screenings: [...((detailsMap[uhid] || {})?.screenings || []), screening]
-          }
-        }
+            screenings: [
+              ...((detailsMap[uhid] || {})?.screenings || []),
+              screening,
+            ],
+          },
+        };
       }
     }
     if ((patientIds || []).length > 0) {
-      const labs = await labItemModel.find({
-        ...dateFilter,
-        patientId: patientIds
-      }).populate([{ path: 'patientId' }])
+      const labs = await labItemModel
+        .find({
+          ...dateFilter,
+          patientId: patientIds,
+        })
+        .populate([{ path: "patientId" }]);
       if ((labs || []).length > 0) {
         for (const item of labs) {
           const uhid = item?.patientId?.uhid;
           detailsMap = {
-            ...detailsMap, [uhid]: {
+            ...detailsMap,
+            [uhid]: {
               ...(detailsMap[uhid] || {}),
-              labItems: [...((detailsMap[uhid] || {})?.labItems || []), item]
-            }
-          }
+              labItems: [...((detailsMap[uhid] || {})?.labItems || []), item],
+            },
+          };
         }
       }
-      console.log(labs.length, 'labs')
     }
     const uhidArray = Object.keys(detailsMap);
 
     // console.log(uhidArray, detailsMap, 're1')
+    let allPdfs = [];
+    let pdfLinks = [];
     for (const uhid of uhidArray) {
+      console.log(pdfLinks, "pdfLinks start");
       const details = detailsMap[uhid];
 
       if (details?.patient?.consolidatedReportUrl) {
-        console.log('Report already generated for :', uhid, details?.patient?.consolidatedReportUrl, details?.campId)
+        console.log(
+          "Report already generated for :",
+          uhid
+          // details?.patient?.consolidatedReportUrl,
+          // details?.campId
+        );
       } else {
-        const pdfLinks = [];
         let labReportGenerated = false;
         let screeningReportGenerated = false;
-        let results = {
-
-        };
+        let results = {};
 
         (details || {}).screenings.map((screening) => {
           if (screening.formsDetails) {
             (screening.formsDetails || []).map((item) => {
-              results = { ...results, ...(item.results || {}) }
-            })
+              results = { ...results, ...(item.results || {}) };
+            });
           }
-        })
+        });
 
         const buffer = await getEjsFile({
-          render, data: tranformerConsolidatedReportData({
+          render,
+          data: tranformerConsolidatedReportData({
             patient: details?.patient,
             resultsObject: results,
             screeningDate: details?.screeningDate,
@@ -125,44 +139,67 @@ module.exports = async (req, res) => {
             district: details.district,
             state: details.state,
           }),
-          fileName: 'consolidated-report'
+          fileName: "consolidated-report" + Date.now(),
         });
         pdfLinks.push(buffer);
         screeningReportGenerated = !!buffer;
 
-
-        for (const lab of (details.labItems || [])) {
-          // console.log(lab.packages, 'reportUrl', '--- || ---');
+        for (const lab of details.labItems || []) {
           if (lab?.packages && lab?.packages[0] && lab?.packages[0].reportUrl) {
-            const labBuffer = await getFileBufferFromUrl(lab?.packages[0].reportUrl);
+            const labBuffer = await getFileBufferFromUrl(
+              lab?.packages[0].reportUrl
+            );
             pdfLinks.push(labBuffer);
             labReportGenerated = true;
           }
         }
 
         if (labReportGenerated && screeningReportGenerated) {
+          console.log(pdfLinks, "pdfLinks");
           const { mergedUrl, error: mergeError } = await pdfMerge({ pdfLinks });
           if (mergeError) {
             console.log(mergeError);
           } else {
-            const patient = await Patient.findByIdAndUpdate(details?.patient?._id, { consolidatedReportUrl: mergedUrl }, { new: true })
-            const campUpdated = await campsModel.findByIdAndUpdate(details?.campId, {
-              $inc: {
-                numberOfConsolidatedReportGenerated: 1
-              }
-            },
+            const patient = await Patient.findByIdAndUpdate(
+              details?.patient?._id,
+              { consolidatedReportUrl: mergedUrl },
+              { new: true }
+            );
+            const campUpdated = await campsModel.findByIdAndUpdate(
+              details?.campId,
               {
-                new: true
+                $inc: {
+                  numberOfConsolidatedReportGenerated: 1,
+                },
+              },
+              {
+                new: true,
               }
             );
-            console.log(patient?.consolidatedReportUrl, '---===---');
-            urlMaps = { ...urlMaps, [patient.uhid]: patient?.consolidatedReportUrl };
-            console.log(Object.keys(urlMaps).length, urlMaps)
+            urlMaps = {
+              ...urlMaps,
+              [patient.uhid]: patient?.consolidatedReportUrl,
+            };
+            console.log(Object.keys(urlMaps).length, urlMaps);
           }
+        } else {
+          pdfLinks = [];
         }
       }
+      allPdfs = [...allPdfs, ...pdfLinks];
+      pdfLinks = [];
     }
 
+    console.log("done..");
+
+    // const { mergedUrl, error: mergeError } = await pdfMerge({
+    //   pdfLinks: allPdfs,
+    // });
+    // if (mergeError) {
+    //   console.log(mergeError)
+    // } else {
+    //   console.log("final", mergedUrl);
+    // }
   } catch (e) {
     console.log(e);
   }
--- utils/upload.js
+++ utils/upload.js
@@ -1,81 +1,78 @@
-const pdf = require('html-pdf');
-const path = require('path');
-const fs = require('fs');
-const aws = require('aws-sdk');
+const pdf = require("html-pdf");
+const path = require("path");
+const fs = require("fs");
+const aws = require("aws-sdk");
 
 const uploadEJSPDF = ({ render, data, fileName }) =>
-    new Promise((resolve, reject) => {
-        const html = render(data);
-        pdf.create(html).toFile((err, file) => {
-            if (err) {
-                console.log('error generating pdf ->', err);
-            } else {
-                const s3 = new aws.S3({
-                    accessKeyId: process.env.S3_ACCESS_KEY,
-                    secretAccessKey: process.env.S3_SECRET,
-                    Bucket: process.env.BUCKET_NAME,
-                });
-                fs.readFile(file.filename, function (err1, data) {
-                    if (err1) {
-                        console.log(err1);
-                        res.status(500).json({ err: 'Could not read file ' });
-                    }
-                    var params = {
-                        ACL: 'public-read',
-                        ContentType: 'application/pdf',
-                        Key:
-                            path
-                                .basename(fileName || 'RegistrationReceipt', path.extname(file.filename))
-                                .trim()
-                                .split(' ')
-                                .join() +
-                            '-' +
-                            Date.now() +
-                            path.extname(file.filename).trim(),
-                        Body: data,
-                        Bucket: process.env.BUCKET_NAME,
-                    };
-                    s3.upload(params, (uploaderr, data1) => {
-                        if (uploaderr) {
-                            console.log(uploaderr);
-                            reject(uploaderr);
-                        }
-                        console.log('Nice work', data1);
-                        fs.unlinkSync(file.filename);
-                        resolve({ link: data1?.Location });
-                    });
-                });
-            }
+  new Promise((resolve, reject) => {
+    const html = render(data);
+    pdf.create(html).toFile((err, file) => {
+      if (err) {
+        console.log("error generating pdf ->", err);
+      } else {
+        const s3 = new aws.S3({
+          accessKeyId: process.env.S3_ACCESS_KEY,
+          secretAccessKey: process.env.S3_SECRET,
+          Bucket: process.env.BUCKET_NAME,
         });
+        fs.readFile(file.filename, function (err1, data) {
+          if (err1) {
+            console.log(err1);
+            res.status(500).json({ err: "Could not read file " });
+          }
+          var params = {
+            ACL: "public-read",
+            ContentType: "application/pdf",
+            Key:
+              path
+                .basename(
+                  fileName || "RegistrationReceipt",
+                  path.extname(file.filename)
+                )
+                .trim()
+                .split(" ")
+                .join() +
+              "-" +
+              Date.now() +
+              path.extname(file.filename).trim(),
+            Body: data,
+            Bucket: process.env.BUCKET_NAME,
+          };
+          s3.upload(params, (uploaderr, data1) => {
+            if (uploaderr) {
+              console.log(uploaderr);
+              reject(uploaderr);
+            }
+            fs.unlinkSync(file.filename);
+            resolve({ link: data1?.Location });
+          });
+        });
+      }
     });
+  });
 
 const getEjsFile = ({ render, data, fileName }) =>
-    new Promise((resolve, reject) => {
-        console.log('called');
-        const html = render(data);
-        console.log('htlm');
-        pdf.create(html).toFile((err, file) => {
-            if (err) {
-
-                console.log('error generating pdf ->', err);
-                reject('error')
-            } else {
-                fs.readFile(file.filename, function (err1, data) {
-                    if (err1) {
-                        console.log(err1);
-                        res.status(500).json({ err: 'Could not read file ' });
-                    } else {
-                        console.log('This is a buffer:', Buffer.isBuffer(data));
-                        resolve(data);
-                    }
-
-                });
-            }
+  new Promise((resolve, reject) => {
+    const html = render(data);
+    pdf.create(html).toFile((err, file) => {
+      if (err) {
+        console.log("error generating pdf ->", err);
+        reject("error");
+      } else {
+        fs.readFile(file.filename, async function (err1, data) {
+          if (err1) {
+            console.log(err1);
+            res.status(500).json({ err: "Could not read file " });
+          } else {
+            await fs.unlinkSync(file.filename);
+            resolve(data);
+          }
         });
+      }
     });
-
+  });
 
 module.exports = {
-    uploadEJSPDF,
-    getEjsFile
-}
+  uploadEJSPDF,
+  getEjsFile,
+};
